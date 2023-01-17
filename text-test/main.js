6906d939d946feff40493b44ce5e22a1d87acae9
sujitjoshiphilips <105477645+sujitjoshiphilips@users.noreply.github.com>
2023-01-17 17:29:25 +0530

    c

--- text-test/main.js
+++ text-test/main.js
@@ -1,125 +1,112 @@
 var currentAlpha = "100";
+let currentHeading = "b";
 let currentBody = "r";
-let currentHeading = "r";
+let modelID = "gltf-inline";
 
 window.onload = function onloadFunction() {
-console.log("onload" + currentAlpha);
+    console.log("onload" + currentAlpha);
 }
 
-            function redAlpha() {
-                
-                
 
-                if(currentAlpha == "100"){
-                    currentAlpha = "50";
-                }
-                else if(currentAlpha == "50"){
-                    currentAlpha = "0";
-                }
-                else if(currentAlpha == "0"){
-                    currentAlpha = "0";
-                }
-                
-                let srcLink = './a'+currentAlpha+'-'+currentHeading+'-'+currentBody+'.glb';
-               document.getElementById('text').setAttribute('src',srcLink); 
-               console.log(currentAlpha+'-'+currentHeading+'-'+currentBody);
-            }
-
-            function incAlpha() {
-                
-                
-
-                if(currentAlpha == "100"){
-                    currentAlpha = "100";
-                }
-                else if(currentAlpha == "50"){
-                    currentAlpha = "100";
-                }
-                else if(currentAlpha == "0"){
-                    currentAlpha = "50";
-                }
-                let srcLink = './a'+currentAlpha+'-'+currentHeading+'-'+currentBody+'.glb';
-               document.getElementById('text').setAttribute('src',srcLink); 
-               console.log(currentAlpha+'-'+currentHeading+'-'+currentBody);
-            }
-
-            function incHead() {
-                
-
-                if(currentHeading == "l"){
-                    currentHeading = "r";
-                }
-                else if(currentHeading == "r"){
-                    currentHeading = "m";
-                }
-                else if(currentHeading == "m"){
-                    currentHeading= "b";
-                }
-                else if(currentHeading == "b"){
-                    currentHeading = "b";
-                }
-
-                let srcLink = './a'+currentAlpha+'-'+currentHeading+'-'+currentBody+'.glb';
-               document.getElementById('text').setAttribute('src',srcLink); 
-               console.log(currentAlpha+'-'+currentHeading+'-'+currentBody);
-            }
-
-            function redHead() {
-
-                if(currentHeading == "l"){
-                    currentHeading = "l";
-                }
-                else if(currentHeading == "r"){
-                    currentHeading = "l";
-                }
-                else if(currentHeading == "m"){
-                    currentHeading= "r";
-                }
-                else if(currentHeading == "b"){
-                    currentHeading = "m";
-                }
-
-                let srcLink = './a'+currentAlpha+'-'+currentHeading+'-'+currentBody+'.glb';
-               document.getElementById('text').setAttribute('src',srcLink); 
-               console.log(currentAlpha+'-'+currentHeading+'-'+currentBody);
-            }
+function redAlpha() {
 
 
-            function incBod() {
 
-                if(currentBody == "l"){
-                    currentBody = "r";
-                }
-                else if(currentBody == "r"){
-                    currentBody = "m";
-                }
-                else if(currentBody == "m"){
-                    currentBody = "b";
-                }
-                else if(currentBody == "b"){
-                    currentBody = "b";
-                }
+    if (currentAlpha == "100") {
+        currentAlpha = "50";
+    } else if (currentAlpha == "50") {
+        currentAlpha = "0";
+    } else if (currentAlpha == "0") {
+        currentAlpha = "0";
+    }
 
-                let srcLink = './a'+currentAlpha+'-'+currentHeading+'-'+currentBody+'.glb';
-               document.getElementById('text').setAttribute('src',srcLink); 
-               console.log(currentAlpha+'-'+currentHeading+'-'+currentBody);
-            }
-            function redBod() {
+    let srcLink = './a' + currentAlpha + '-' + currentHeading + '-' + currentBody + '.glb';
+    document.getElementById(modelID).setAttribute('gltf-model', srcLink);
+    console.log(currentAlpha + '-' + currentHeading + '-' + currentBody);
+}
 
-                if(currentBody == "l"){
-                    currentBody = "l";
-                }
-                else if(currentBody == "r"){
-                    currentBody = "l";
-                }
-                else if(currentBody == "m"){
-                    currentBody = "r";
-                }
-                else if(currentBody == "b"){
-                    currentBody = "m";
-                }
+// gltf-model="url(./a100-b-r.glb)"
 
-                let srcLink = './a'+currentAlpha+'-'+currentHeading+'-'+currentBody+'.glb';
-               document.getElementById('text').setAttribute('src',srcLink); 
-               console.log(currentAlpha+'-'+currentHeading+'-'+currentBody);
-            }
+function incAlpha() {
+
+    if (currentAlpha == "100") {
+        currentAlpha = "100";
+    } else if (currentAlpha == "50") {
+        currentAlpha = "100";
+    } else if (currentAlpha == "0") {
+        currentAlpha = "50";
+    }
+    let srcLink = './a' + currentAlpha + '-' + currentHeading + '-' + currentBody + '.glb';
+    document.getElementById(modelID).setAttribute('gltf-model', srcLink);
+    console.log(currentAlpha + '-' + currentHeading + '-' + currentBody);
+}
+
+function incHead() {
+
+
+    if (currentHeading == "l") {
+        currentHeading = "r";
+    } else if (currentHeading == "r") {
+        currentHeading = "m";
+    } else if (currentHeading == "m") {
+        currentHeading = "b";
+    } else if (currentHeading == "b") {
+        currentHeading = "b";
+    }
+
+    let srcLink = './a' + currentAlpha + '-' + currentHeading + '-' + currentBody + '.glb';
+    document.getElementById(modelID).setAttribute('gltf-model', srcLink);
+    console.log(currentAlpha + '-' + currentHeading + '-' + currentBody);
+}
+
+function redHead() {
+
+    if (currentHeading == "l") {
+        currentHeading = "l";
+    } else if (currentHeading == "r") {
+        currentHeading = "l";
+    } else if (currentHeading == "m") {
+        currentHeading = "r";
+    } else if (currentHeading == "b") {
+        currentHeading = "m";
+    }
+
+    let srcLink = './a' + currentAlpha + '-' + currentHeading + '-' + currentBody + '.glb';
+    document.getElementById(modelID).setAttribute('gltf-model', srcLink);
+    console.log(currentAlpha + '-' + currentHeading + '-' + currentBody);
+}
+
+
+function incBod() {
+
+    if (currentBody == "l") {
+        currentBody = "r";
+    } else if (currentBody == "r") {
+        currentBody = "m";
+    } else if (currentBody == "m") {
+        currentBody = "b";
+    } else if (currentBody == "b") {
+        currentBody = "b";
+    }
+
+    let srcLink = './a' + currentAlpha + '-' + currentHeading + '-' + currentBody + '.glb';
+    document.getElementById(modelID).setAttribute('gltf-model', srcLink);
+    console.log(currentAlpha + '-' + currentHeading + '-' + currentBody);
+}
+
+function redBod() {
+
+    if (currentBody == "l") {
+        currentBody = "l";
+    } else if (currentBody == "r") {
+        currentBody = "l";
+    } else if (currentBody == "m") {
+        currentBody = "r";
+    } else if (currentBody == "b") {
+        currentBody = "m";
+    }
+
+    let srcLink = './a' + currentAlpha + '-' + currentHeading + '-' + currentBody + '.glb';
+    document.getElementById(modelID).setAttribute('gltf-model', srcLink);
+    console.log(currentAlpha + '-' + currentHeading + '-' + currentBody);
+}
