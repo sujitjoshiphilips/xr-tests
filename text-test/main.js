3928b936585af7aa493dbb312927b0ce64ec47af
sujitjoshiphilips <105477645+sujitjoshiphilips@users.noreply.github.com>
2023-01-18 17:27:23 +0530

    c

--- text-test/main.js
+++ text-test/main.js
@@ -2,9 +2,9 @@ var currentAlpha = "100";
 let currentHeading = "b";
 let currentBody = "r";
 let modelID = "gltf-inline";
-let sx = 0.1;
-let sy = 0.1;
-let sz = 0.1;
+let sx = 0.083;
+let sy = 0.083;
+let sz = 0.083;
 
 window.onload = function onloadFunction() {
     console.log("onload" + currentAlpha);
